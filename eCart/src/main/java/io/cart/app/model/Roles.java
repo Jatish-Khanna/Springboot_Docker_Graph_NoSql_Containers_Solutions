51d3167fd330785a5be52f8416524c7d5dc3114d
Jatish Khanna <jatish.khanna@gmail.com>
2019-11-26 15:04:20 +0530

    Create Roles.java

---
+++ eCart/src/main/java/io/cart/app/model/Roles.java
@@ -0,0 +1,21 @@
+package io.cart.app.model;
+
+import lombok.Data;
+
+/**
+ * Role Model - data transfer class
+ * 
+ * @author Jatish_Khanna
+ *
+ */
+
+
+// * Lombok annotations starts -
+// * provides implementation to mentioned methods
+@Data
+public class Roles {
+
+  private String roleId;
+  private String roleName;
+
+}

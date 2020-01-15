b53208bb327b378b72717b3ba64b9056ff57ad20
Jatish Khanna <jatish.khanna@gmail.com>
2020-01-15 13:14:02 +0530

    Create GraphqlStartApplication.java

---
+++ graphql-start/src/main/java/io/graphql/start/graphqlstart/GraphqlStartApplication.java
@@ -0,0 +1,14 @@
+package io.graphql.start.graphqlstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class  GraphqlStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlStartApplication.class, args);
+	}
+
+}

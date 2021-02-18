b6f5d72a8c071171aef0b930de48bf7cee1605e2
Jatish Khanna <jatish.khanna@gmail.com>
2021-02-18 08:55:17 +0530

    Create SecuritiyConfiguration.java

---
+++ SecuritiyConfiguration.java
@@ -0,0 +1,12 @@
+
+ // Removes the CORS error if the Credentials are Ok
+@Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+            .antMatchers(HttpMethod.OPTIONS, "/**")
+            .antMatchers("/app/**/*.{js,html}")
+            .antMatchers("/i18n/**")
+            .antMatchers("/content/**")
+            .antMatchers("/swagger-ui/index.html")
+            .antMatchers("/test/**");
+    }
